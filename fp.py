1be489fbbcdf98135963ad6f04e2ac06ad4693e0
yancz1989 <yancz1989@gmail.com>
2017-02-05 00:35:00 +0800

    fix bugs

--- fp.py
+++ fp.py
@@ -2,7 +2,7 @@
 # @Author: yancz1989
 # @Date:   2017-02-03 16:18:59
 # @Last Modified by:   yancz1989
-# @Last Modified time: 2017-02-04 17:48:53
+# @Last Modified time: 2017-02-05 00:25:20
 
 import tensorflow as tf
 import numpy as np
@@ -52,14 +52,17 @@ def build(H, x, training = True):
   return logits, pred
 
 def train(args):
-  logging.basicConfig(filename = 'running.log', 
-        format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG)
   with open(args.hype) as f:
     H = json.load(f)
     H['subset'] = args.subset
     H['save_dir'] = OUTPUT_DIR + 'subset' + str(H['subset'])
+    if args.gpu != None:
+      H['gpu'] = args.gpu
   with open(META_DIR + 'fp.json') as fpj:
     meta = json.load(fpj)
+
+  logging.basicConfig(filename = 'fp_detect_' + str(H['subset']) + '.log', 
+        format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG)
 
   bsize = H['batch_size']
   x = tf.placeholder(tf.float32, shape = [64, 64, 1])
@@ -67,7 +70,7 @@ def train(args):
 
   q, eq_op= {}, {}
   for training in ['train', 'valid']:
-    q[training] = tf.FIFOQueue(capacity = bsize * 4, dtypes = [tf.float32, tf.int64],
+    q[training] = tf.FIFOQueue(capacity = bsize * 2, dtypes = [tf.float32, tf.int64],
         shapes = ([64, 64, 1], []))
     eq_op[training] = q[training].enqueue([x, y])
   Xt, Yt = tf.train.batch(q['train'].dequeue(), batch_size = bsize, capacity = bsize * 2)
@@ -108,7 +111,7 @@ def train(args):
 
   vTN = tf.reduce_sum(tf.cast(tf.not_equal(pred, Yt) & tf.equal(Yt, 1), tf.float32))
   vFP = tf.reduce_sum(tf.cast(tf.not_equal(pred, Yt) & tf.equal(Yt, 0), tf.float32))
-  vacc = tf.reduce_sum(tf.cast(tf.equal(pred, Yt), tf.float32)) / tf.size(Yt, out_type = tf.float32)
+  vAcc = tf.reduce_sum(tf.cast(tf.equal(pred, Yt), tf.float32)) / tf.size(Yt, out_type = tf.float32)
   opts = {
       'RMS': tf.train.RMSPropOptimizer,
       'Adam': tf.train.AdamOptimizer,
@@ -128,9 +131,9 @@ def train(args):
   tf.summary.scalar('acc', acc)
   tf.summary.scalar('vTN', vTN)
   tf.summary.scalar('vFP', vFP)
-  tf.summary.scalar('vacc', vacc)
+  tf.summary.scalar('vacc', vAcc)
   tf.summary.scalar('norm', tf.reduce_sum(norm))
-
+  summary_op = tf.merge_all_summaries()
 
   os.environ['CUDA_VISIBLE_DEVICES'] = str(H['gpu'])
   gpu_options = tf.GPUOptions()
@@ -162,6 +165,8 @@ def train(args):
         if step % 100 == 0:
           logging.info('Training batchs %d, avg loss %f, acc %f, TN %d/%d, FP %d/%d.' % 
             (step, tol_loss / step, tol_acc / step, tol_ttn, step * bsize, tol_tfp, step * bsize))
+          summary_str = sess.run(summary_op)
+          writer.add_summary(summary_str, global_step = gstep.eval())
         tol_loss += tloss
         tol_ttn += tn
         tol_tfp += fp
@@ -169,17 +174,20 @@ def train(args):
 
       for step in range(valid_batches):
         curX, curY = sess.run([Xv, Yv])
-        vtn, vfp, vacc = sess.run([vTN, vFP, vacc], feed_dict = {Xt : curX, Yt: curY})
+        vtn, vfp, vacc = sess.run([vTN, vFP, vAcc], feed_dict = {Xt : curX, Yt: curY})
         tol_vtn += vtn
         tol_vfp += vfp
         tol_vacc += vacc
 
-      logging.info('')
       t = time.time() - tst
-      logging.info(('epoch %d, time elapse %f, training loss %f,',
-        ' valid avg TN %d/%d, %d/%d, acc %f.') % (epoch, t, tol_loss / (train_batches * bsize),
-        float(tol_vtn) / (valid_batches * bsize), float(tol_vfp) / (valid_batches * bsize)))
-      saver.save(sess, OUTPUT_DIR + 'subset' + H['subset'] + '/save.ckpt')
+      print((epoch, t, tol_loss / (train_batches * bsize),
+        float(tol_vtn) / (valid_batches * bsize), float(tol_vfp) / (valid_batches * bsize),
+        tol_vacc / valid_batches))
+      logging.info(('epoch %d, time elapse %f, training loss %f,' + 
+        ' valid avg TN %f, FP %f, acc %f.') % (epoch, t, tol_loss / (train_batches * bsize),
+        float(tol_vtn) / (valid_batches * bsize), float(tol_vfp) / (valid_batches * bsize),
+        tol_vacc / valid_batches))
+      saver.save(sess, OUTPUT_DIR + 'subset' + str(H['subset']) + '/save.ckpt', global_step = gstep)
 
     logging.info('training finished, try ending...')
     sess.run(q.close(cancel_pending_enqueues=True))
@@ -197,6 +205,7 @@ def main():
   parser.add_argument('--weight', default = None, type = str)
   parser.add_argument('--subset', default = None, type = int)
   parser.add_argument('--train', default = 1, type = int)
+  parser.add_argument('--gpu', default = None, type = int)
   args = parser.parse_args()
   if args.hype == None or args.subset == None:
     raise 'Error: Config file and subset need to be specified.'
